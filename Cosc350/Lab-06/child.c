d524d785b53f3c6f20b3ef8461d9418a98fbf36a
Cody Murrer <codymurrer@gmail.com>
2021-04-12 18:58:17 -0400

    fixed tabbing on forkExec.c and child.c

--- Cosc350/Lab-06/child.c
+++ Cosc350/Lab-06/child.c
@@ -6,37 +6,31 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int stringtoint(char *c)
-{
-  int i = 0;
-  long int num = 0;
-  while (c[i] != '\0')
-  {
-    num = 10 * num + (c[i] - '0');
-    i++;
-  }
-  return num;
+int stringtoint(char *c){
+    int i = 0;
+    long int num = 0;
+    while (c[i] != '\0'){
+        num = 10 * num + (c[i] - '0');
+        i++;
+    }
+    return num;
 }
 
-int main(int argc, char *argv[])
-{
-  if (argc != 3)
-  {
-    printf("ERROR: Expected 3 arguments!\n");
-    printf("Example: ./child [message] [Nc] [Tc]\n");
-    return -1;
-  }
-  pid_t pid = getpid();
-  char *message = argv[0];
-  int Nc = stringtoint(argv[1]);
-  int Tc = stringtoint(argv[2]);
-  int n = Nc;
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        printf("ERROR: Expected 3 arguments!\n");
+        printf("Example: ./child [message] [Nc] [Tc]\n");
+        return -1;
+    }
+    pid_t pid = getpid();
+    char *message = argv[0];
+    int Nc = stringtoint(argv[1]);
+    int Tc = stringtoint(argv[2]);
+    int n = Nc;
 
-  for (; n > 0; n--)
-  {
-    printf("%s | pid: %d\n", message, pid);
-    sleep(Tc);
-  }
-
-  exit(37);
+    for (; n > 0; n--){
+        printf("%s | pid: %d\n", message, pid);
+        sleep(Tc);
+    }
+    exit(37);
 }
--- Cosc350/Lab-06/forkExec.c
+++ Cosc350/Lab-06/forkExec.c
@@ -5,58 +5,57 @@
 #include <stdlib.h>
 
 int stringtoint(char *c){
-  int i = 0;
-  long int num = 0;
-  while (c[i] != '\0')
-  {
-    num = 10 * num + (c[i] - '0');
-    i++;
-  }
-  return num;
+    int i = 0;
+    long int num = 0;
+    while (c[i] != '\0'){
+        num = 10 * num + (c[i] - '0');
+        i++;
+    }
+    return num;
 }
 
 int main(int argc, char *argv[]){
-  pid_t pid;
-  char *message;
-  int n;
-  if (argc != 5){
-    printf("ERROR: Invalid number of arguments passed, expected 4\n");
-    printf("Example: ./fork [Nc] [Np] [Tc] [Tp]\n");
-    return -1;
-  }
-
-  int Nc = stringtoint(argv[1]);
-  int Np = stringtoint(argv[2]);
-  int Tc = stringtoint(argv[3]);
-  int Tp = stringtoint(argv[4]);
-
-  printf("fork program starting\n");
-  pid = fork();
-
-  int sleepTime;
-
-  switch (pid){
-  case -1:
-    perror("fork failed");
-    exit(1);
-  case 0:
-    printf("Exec returned: %d\n", execlp("./child", "This is the child", argv[1], argv[3], (char *)0));
-    break;
-  default:
-    message = "This is the parent";
-    n = Np;
-    sleepTime = Tp;
-    break;
-  }
-
-  for (; n > 0; n--){
-    printf("%s pid: %d\n", message, pid);
-    sleep(sleepTime);
-    if (pid != 0){
-      printf("Waiting for child process to terminate.\n");
-      wait(0);
-      printf("Child process terminated.\n");
+    pid_t pid;
+    char *message;
+    int n;
+    if (argc != 5){
+        printf("ERROR: Invalid number of arguments passed, expected 4\n");
+        printf("Example: ./fork [Nc] [Np] [Tc] [Tp]\n");
+        return -1;
     }
-  }
-  return 0;
+
+    int Nc = stringtoint(argv[1]);
+    int Np = stringtoint(argv[2]);
+    int Tc = stringtoint(argv[3]);
+    int Tp = stringtoint(argv[4]);
+
+    printf("fork program starting\n");
+    pid = fork();
+
+    int sleepTime;
+
+    switch (pid){
+        case -1:
+            perror("fork failed");
+            exit(1);
+        case 0:
+            printf("Exec returned: %d\n", execlp("./child", "This is the child", argv[1], argv[3], (char *)0));
+            break;
+        default:
+            message = "This is the parent";
+            n = Np;
+            sleepTime = Tp;
+            break;
+    }
+
+    for (; n > 0; n--){
+        printf("%s pid: %d\n", message, pid);
+        sleep(sleepTime);
+        if (pid != 0){
+            printf("Waiting for child process to terminate.\n");
+            wait(0);
+            printf("Child process terminated.\n");
+        }
+    }
+    return 0;
 }

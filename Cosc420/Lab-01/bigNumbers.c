e4a051fe2075c5e587728476ab12e2d150309ac9
ckmurrer <codymurrer@gmail.com>
2021-09-09 09:27:39 -0400

    Bug fix lab-01

--- Cosc420/Lab-01/bigNumbers.c
+++ Cosc420/Lab-01/bigNumbers.c
@@ -23,10 +23,10 @@ int main(int argc, char** argv){
     MPI_Comm_size(world, &wSize);
     MPI_Comm_rank(world, &rank);
 
-    unsigned long int usrInput = atoll(argv[1]);
-    int rootInput = sqrt(usrInput);
+    unsigned long long usrInput = atoll(argv[1]);
+    unsigned long long rootInput = sqrt(usrInput);
 
-    int startSpot, endSpot;
+    unsigned long long startSpot, endSpot;
     endSpot = (((rank+1)*rootInput)/wSize); // formula given by Dr. Anderson during office hours.
     // checks if rank is 0 and if so will make loop start at two when it is ran for rank 0.
     if(rank == 0){
@@ -34,16 +34,17 @@ int main(int argc, char** argv){
     }else{
         startSpot = ((rank*rootInput)/wSize)+1;// formula given by Dr. Anderson during office hours added one to have node start on k+1 .
     }
-    printf("Rank: %d starts at: %d, and ends at: %d.\n",rank,startSpot,endSpot); // prints the range of number the range checks.
+    printf("Rank: %d starts at: %llu, and ends at: %llu.\n",rank,startSpot,endSpot); // prints the range of number the range checks.
 
-    int i, divFound;
+    int divFound;
+    unsigned long long i;
     for(i=startSpot; i<=endSpot; i++){
         // checks if the number is prime and if so then print out the first divisor for each rank that is found then break.
         if(i == 1||i == 0){
             continue;
         }
         if(usrInput%i==0){
-            printf("DIVISOR FOUND... Rank: %d found a divisor of %d.\n",rank,i); // prints found divisor.
+            printf("DIVISOR FOUND... Rank: %d found a divisor of %llu.\n",rank,i); // prints found divisor.
             divFound = 1;
             break;
         }else{

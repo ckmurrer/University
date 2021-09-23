77d8d8db7c1d30a3dfa90df23474083c018d3da8
Cody Murrer <codymurrer@gmail.com>
2021-09-22 21:13:48 -0400

    Started matrix.c/h

--- Cosc420/Lab-02/matrix.h
+++ Cosc420/Lab-02/matrix.h
@@ -1,6 +1,21 @@
-#ifdef MATRIX_H
+#ifndef MATRIX_H
 #define MATRIX_H
 
+#define INDEX(i,j,n,m) i*m+j
+#define ACCESS(A,i,j) A.data[INDEX(i,j,A.rows,A.cols)]
 
+typedef struct matrix{
+    int rows;
+    int cols;
+    int* data;
+}matrix;
+
+// addition
+
+// subtraction
+
+// multiplication
+
+// innerproduct
 
 #endif

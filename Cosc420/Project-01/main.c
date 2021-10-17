a476024584a4d28e7a57bd697ad85e341a458c16
Cody Murrer <codymurrer@gmail.com>
2021-10-17 16:47:50 -0400

    checkpoint

--- Cosc420/Project-01/main.c
+++ Cosc420/Project-01/main.c
@@ -15,128 +15,215 @@
 
 /*	
 	TODO:
-		figure out how to read the text docs and send windows of data to nodes
-		let every node open the hash text doc
-		figure out how to split hashed text from usernames (possibly by using colon ascii code)
-		create the suffix and prefix methods from 0000-9999
+		create the suffix from 0000-9999
 		create a hashing method that will append the suffix or prefix to the dictionary word and compare with hashed password from shadow file
 		figure out how to tell other nodes a hash has been found and not to check that one anymore 
 		figure our how to possibly make save points
 */
 
-
-// does not exceed 4 characters
-void suffix(){
-
+int char_to_int(char* input){
+    int trans = 0; 
+    int pos = 0;
+// checks to see if there is a char in the input stream
+// that can be translated into ascii
+    while(input[pos] != '\0'){
+       trans = (10 * trans) + (input[pos]-'0');
+       pos++;
+    }
+    return trans;
 }
+// calls the above method to change the passed char to an int value
+// then changes that int value to a corresponding char then returns
+char int_to_char(char *input){
+    int number = char_to_int(input); // calls the char to int conversion method
+    char textVal = number; 
+    return textVal;
+}
+
+
 // does not exceed 4 characters
-void prefix(char dicWord[],int start, int end, int rank){
-	char buffer[255] ;
+void suffix(char buff []){
+	printf("%s",buff);
+}
+
+// does not exceed 4 characters
+char prefix(char dicWord[],int start, int end, int rank,int wSize,char stopOp[]){
+	char buffer[255];
 	int count = 0, inc = 0;
+	int stop = start*wSize;
 	int	temp = start,temp2;
-	int arr[100];
+	//char stopOp[100];
+	char arr[100];
+	int arr2[100];
+	int x = 0;
+
 // partitions the amount of operations needed per node rank*(9999/wSize)	
 	if(start <10){
 		sprintf(buffer,"0%s",dicWord);
 		buffer[0]+=start;	
 	}
-	while(temp!=0){
-		if(start >9 && start < 100){
-			sprintf(buffer,"00%s",dicWord);
-			if(inc == 2){
-				temp = 0;
+		while(temp!=0){
+			if(start >9 && start < 100){
+				sprintf(buffer,"00%s",dicWord);
+				if(inc == 2){
+					temp = 0;
+				}
+				temp2 = temp % 10;
+				arr2[inc] = temp2;
+				arr[inc] = arr2[inc];
+				buffer[0]+=arr[1];
+				buffer[1]+=arr[0];
+				temp = temp/10;
+				inc++;
+			}	
+			if(start >99 && start < 1000){
+				sprintf(buffer,"000%s",dicWord);
+				if(inc == 3){
+					temp = 0;
+				}
+				temp2 = temp % 10;
+				arr[inc] = temp2;
+				buffer[0]+=arr[2];
+				buffer[1]+=arr[1];
+				buffer[2]+=arr[0];
+				temp = temp/10;
+				inc++;
 			}
-			temp2 = temp % 10;
-			arr[inc] = temp2;
-			buffer[0]+=arr[1];
-			buffer[1]+=arr[0];
-			temp = temp/10;
-			inc++;
-		}	
-		if(start >99 && start < 1000){
-			sprintf(buffer,"000%s",dicWord);
-			if(inc == 3){
-				temp = 0;
+			if(start >999 && start < 10000){
+					sprintf(buffer,"0000%s",dicWord);
+				if(inc == 4){
+					temp = 0;
+				}
+				temp2 = temp % 10;
+				arr2[inc] = temp2;
+				arr[inc] = arr2[inc];
+				buffer[0]+=arr[3];
+				buffer[1]+=arr[2];
+				buffer[2]+=arr[1];
+				buffer[3]+=arr[0];
+				temp = temp/10;
+				inc++;
 			}
-			temp2 = temp % 10;
-			arr[inc] = temp2;
-			buffer[0]+=arr[2];
-			buffer[1]+=arr[1];
-			buffer[2]+=arr[0];
-			temp = temp/10;
-			inc++;
 		}
-		if(start >999 && start < 10000){
-				sprintf(buffer,"0000%s",dicWord);
-			if(inc == 4){
-				temp = 0;
-			}
-			temp2 = temp % 10;
-			arr[inc] = temp2;
-			buffer[0]+=arr[3];
-			buffer[1]+=arr[2];
-			buffer[2]+=arr[1];
-			buffer[3]+=arr[0];
-			temp = temp/10;
-			inc++;
-		}
-	}
-
-	int i,k,pls=0,work=0,maybe = 0,boop = 0, check = 0;
+	int i,k,pls=0,work=0,maybe = 0,boop = start, check = start;
 	for(k = start; k < end;k++){
-		count = 0;
-		for(i=0; i< 10; i++){
-			if(count!=10&&k==0){
-			printf("%s",buffer);
-				buffer[0]++;
-				count++;
-			}
-			if(k<10 && k!=0){
-				if(count==0&&k>1){
-					buffer[1] -= 10;
+		for(i=0; i < 10; i++){
+			if(k<9){
+				count = 0;
+				if(count!=10&&k==start){
+					suffix(buffer);
+					buffer[0]++;
+					x++;
 					count++;
 				}
-				if(i<10){
-					printf("%s",buffer);
+			}
+			if(k>start+9 && k < start+20){
+				if(x==10){
+					sprintf(buffer,"00%s",dicWord);
+					count = 0;
+					x++;
+					
+				}
+				if(maybe == 0 && x!=11){
+					buffer[1] -= arr2[0];
+					count = 0;
+					maybe ++;
+				}
+				if(count == 0 && k!=start+10){
+					buffer[1]-=10;
+				}		
+				if(count != 10){
+					suffix(buffer);
 					buffer[1]++;
+					count++;
+				}
+				if(count == 10){
+					buffer[0]++;
+					count = 0;
+				}
+				/*if(pls==0){
+					sprintf(buffer,"00%s",dicWord);
+					count = 0;
+					pls++;
+				}
+				if(count == 0 && k!=start+10){
+					buffer[1]-=10;
+				}		
+				if(count != 10){
+					suffix(buffer);
+					buffer[1]++;
+					count++;
+				}
+				if(count == 10){
+					buffer[0]++;
+					count = 0;
+				}*/
+			}/*
+			if(k>start+19 && k<start+100){
+				if(pls==1){
+					sprintf(buffer,"000%s",dicWord);
+					count = 0;
+					pls++;
+				}
+
+				if(count == 0 && k!=start+20){
+					buffer[2]-=10;
+				}	
+				if(count != 10){
+					suffix(buffer);
+					buffer[2]++;
+					count++;
+				}
+				if(count == 10){
+					buffer[1]++;
+					work++;
+					count = 0;
+				}
+				if(work == 10){
+					buffer[0]++;
+					buffer[1]-=10;
+					work = 0;
 				}
 			}
+			if(k>start+99 && k<start+1000){
+				if(pls==2){
+					sprintf(buffer,"0000%s",dicWord);
+					count = 0;
+					pls++;
+				}
 
-		}
-		if(k<10){
-			if(pls==0){
-				sprintf(buffer,"00%s",dicWord);
-				pls++;
-			}
-			
-			if(maybe!=0){
-				if(work!=10){
-					buffer[0]++;
-					work++;
+				if(count == 0 && k!=start+100){
+					buffer[3]-=10;
 				}	
-			}
-			maybe++;
+				if(count != 10){
+					suffix(buffer);
+					buffer[3]++;
+					count++;
+				}
+				if(count == 10){
+					buffer[2]++;
+					work++;
+					count = 0;
+				}
+				if(work == 10){
+					buffer[1]++;
+					buffer[2]-=10;
+					boop++;
+					work = 0;
+				}
+				if(boop == 10){
+					buffer[0]++;
+					buffer[1]-=10;
+					boop = 0;
+				}
+			}*/
+		}	
+		if(buffer[0] == stopOp[0]){
+			break;
 		}
-/*
-
-		if(k>10&&k<20){
-			if(boop==0){
-				sprintf(buffer,"000%s",dicWord);
-				boop++;
-			}
-			if(check!=9){
-				buffer[0]++;
-				check++;
-			}
-		}
-		*/
+		//printf("%s",stopOp);
 	}
-}
-
-void test(char shadow[], int start, int end, int rank){
-	int temp = 2000;
-	prefix(shadow,start,end, rank);
-	//printf("Rank: %d --- %s",rank,shadow);
+	
 }
 
 int main(int argc, int** argv){
@@ -189,19 +276,28 @@ int main(int argc, int** argv){
 		i++;
 	}
 
-	int maxSize = 9999;
+	int maxSize = 50;
 	int operations = (maxSize/wSize);
 	int start = rank*operations;
 	int end = start+operations;
 
+	int try = end;
+	char t2;
+	char stopPoint[4];
+	while(try){
+		t2=try%10;
+		try/=10;
+	}
+	sprintf(stopPoint,"%d",t2);
+	
 	int j = 0;
-	FILE* dictionary = fopen("words.txt","r");
+	FILE* dictionary = fopen("test.txt","r");
 	while(fgets(dicBuffer,255,dictionary)){
 		strcpy(words[0],dicBuffer);
-		test(words[0],start,end, rank);
+		prefix(words[0],start,end, rank,wSize, stopPoint);
 	}
 
-//	printf("rank = %d\nmax = %d\noperations = %d\nstart = %d\nend = %d\n",rank,maxSize,operations,start,end);
+	//printf("rank = %d\nmax = %d\noperations = %d\nstart = %d\nend = %d\ntemp = %s\n",rank,maxSize,operations,start,end,stopPoint);
 
 	fclose(dictionary);
 	fclose(shadow);
